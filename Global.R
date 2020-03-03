69dafd5637e2bebfd76329142ee6d78871d108aa
Evan <evanmurray2012@gmail.com>
2020-03-03 15:43:41 +0000

    Added france

--- Global.R
+++ Global.R
@@ -1,17 +1,30 @@
 path = "C:/Users/user/OneDrive/FYP/Data"
-path2 = "C:/Users/user/OneDrive/FYP/DataUK"
+pathUk = "C:/Users/user/OneDrive/FYP/DataUK"
+pathFr = "C:/Users/user/OneDrive/FYP/DataFrance"
 
 file <- dir(path, pattern = ".geojson", full.names = TRUE)
-file2 <- dir(path2, pattern = ".geojson", full.names = TRUE)
+fileUk <- dir(pathUk, pattern = ".geojson", full.names = TRUE)
+fileFr <- dir(pathFr, pattern = ".geojson", full.names = TRUE)
+
 
 counties <- geojsonio::geojson_read(file, what = "sp")
-england <- geojsonio::geojson_read(file2, what = "sp")
+england <- geojsonio::geojson_read(fileUk, what = "sp")
+france <- geojsonio::geojson_read(fileFr, what = "sp")
+
+france$population
+france$name
 
 
-binsPop <- c(20000, 40000, 80000, 100000, 150000, 180000, 200000,
+binsPopFr <- c(20000, 40000, 80000, 100000, 150000, 180000, 200000,
              250000, 500000, 1000000, Inf)
+
+binsPopFr <- c(200, 20000, 1000000, 2000000, 3000000, 5000000, 6000000, 7000000, 10000000, Inf)
+
 palPop <-
   colorBin("YlOrRd", domain = counties$population, bins = binsPop)
+
+palPopFr <-
+  colorBin("YlOrRd", domain = france$name, bins = binsPopFr)
 
 
 labelsPop <- sprintf("<strong>%s</strong><br/>%g people",
@@ -24,4 +37,9 @@ palDen <- colorBin("YlOrRd", domain = counties$id, bins = binsDen)
 labelsDen <- sprintf(
   "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
   counties$name, counties$density
+) %>% lapply(htmltools::HTML)
+
+labelsFr <- sprintf(
+  "<strong>%s</strong><br/>%g people",
+  france$name, france$population
 ) %>% lapply(htmltools::HTML)
