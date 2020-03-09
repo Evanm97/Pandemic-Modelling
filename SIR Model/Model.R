469840227a80e85887c7bc64bd5ace517a97870e
Evan <evanmurray2012@gmail.com>
2020-03-09 14:14:25 +0000

    Added quarantine to model

--- SIR Model/Model.R
+++ SIR Model/Model.R
@@ -9,12 +9,18 @@ model <- function(time, stocks, auxs){
     fIR <- sSusceptible * aLambda
     fRR <- sInfected / aDelay
     
+    # Quaratine
+    fQR <- fIR * quarantineF
+    fQRR <- fQR / aDelay
+    
+    d_sQuaratined_dt   <- fQR - fQRR
     d_sSusceptible_dt  <- -fIR
-    d_sInfected_dt     <- fIR - fRR
-    d_sRecovered_dt    <- fRR
+    d_sInfected_dt     <- fIR - fRR - fQR
+    d_sRecovered_dt    <- fRR + fQRR
     
     return (list(c(d_sSusceptible_dt,d_sInfected_dt,d_sRecovered_dt),
                  IR=fIR, RR=fRR,Lambda=aLambda,
                  CE=aEffectiveContactRate,RecDelay=aDelay))   
   })
 }
+
